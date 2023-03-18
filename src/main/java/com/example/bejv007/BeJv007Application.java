6baad2417f186006cd7fabd89aa9bbbe7d7a6422
Ana Beatriz Trindade <anabeatrizdmt@hotmail.com>
2023-03-17 21:02:54 -0300

    Starting the project

---
+++ src/main/java/com/example/bejv007/BeJv007Application.java
@@ -0,0 +1,13 @@
+package com.example.bejv007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJv007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJv007Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bejv007/BeJv007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bejv007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJv007ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
