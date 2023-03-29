fa80d0b417141a1eea06bf04d0d30ddfe4892b88
Antonio Neto <antonioneto.mec@gmail.com>
2023-03-29 16:05:35 -0300

    AuthenticationRestController

---
+++ src/main/java/com/example/bejv007/user/RoleENUM.java
@@ -0,0 +1,6 @@
+package com.example.bejv007.user;
+
+public enum RoleENUM {
+    ADMIN,
+    CLIENT
+}
--- src/main/java/com/example/bejv007/user/UserModel.java
+++ src/main/java/com/example/bejv007/user/UserModel.java
@@ -4,14 +4,25 @@ import com.example.bejv007.user.dto.UserDTO;
 import com.example.bejv007.user.entities.RoleEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 
 import java.util.Collection;
+
+
 
 @Entity
 @Getter @Setter @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,8 +30,10 @@ public class UserModel {
     private String username;
     private String email;
     private String password;
-    private boolean enabled;
-    private boolean tokenExpired;
+
+    @Enumerated(EnumType.STRING)
+    private RoleENUM role;
+
 
     public UserModel(String username, String email, String password) {
         this.username = username;
@@ -29,7 +42,7 @@ public class UserModel {
     }
 
     public static UserModel from(UserDTO userDTO) {
-        return new UserModel(userDTO.getName(), userDTO.getEmail(), userDTO.getPassword());
+        return new UserModel(userDTO.getUsername(), userDTO.getEmail(), userDTO.getPassword());
     }
 
     public static UserResponse userModelToUserResponse(UserModel userModel){
@@ -45,4 +58,40 @@ public class UserModel {
                     name = "role_id", referencedColumnName = "id"))
     private Collection<RoleEntity> roles;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
